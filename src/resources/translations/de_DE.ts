0805097525e8225cf84f95f1dfcd65847a9c03a3
Herbert Thielen <thielen@hs-worms.de>
2016-07-18 16:53:29 +0200

    add some Qt Translator tests

--- src/resources/translations/de_DE.ts
+++ src/resources/translations/de_DE.ts
@@ -1,4 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../unittest/testTranslation.cpp" line="+20"/>
+        <location line="+15"/>
+        <location line="+14"/>
+        <location line="+14"/>
+        <source>Welcome!</source>
+        <translation>Willkommen!</translation>
+    </message>
+</context>
 </TS>
--- src/resources/translations/fr_FR.ts
+++ src/resources/translations/fr_FR.ts
@@ -1,4 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../unittest/testTranslation.cpp" line="+20"/>
+        <location line="+15"/>
+        <location line="+14"/>
+        <location line="+14"/>
+        <source>Welcome!</source>
+        <translation>Bienvenue!</translation>
+    </message>
+</context>
 </TS>
