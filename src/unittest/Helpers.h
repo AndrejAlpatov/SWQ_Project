d1d24f90412202f2777ee07be91554d7720bc3e6
Herbert Thielen <thielen@hs-worms.de>
2016-07-18 14:47:15 +0200

    add helper functions for EXPECT_STREQ on QString

--- src/unittest/Helpers.h
+++ src/unittest/Helpers.h
@@ -24,5 +24,61 @@ namespace Helpers {
     }
 }
 
+#include <gtest/gtest.h>
+// add googletest helpers for QString
+namespace testing {
+    namespace internal {
+        // Helper functions for ASSERT_STREQ / EXPECT_STREQ on QStrings.
+        inline
+        AssertionResult CmpHelperSTREQ(const char* expected_expression,
+                                       const char* actual_expression,
+                                       const char* expected,
+                                       const QString & actual) {
+            return CmpHelperSTREQ(expected_expression, actual_expression, expected, actual.toUtf8().data());
+        }
+
+        inline
+        AssertionResult CmpHelperSTREQ(const char* expected_expression,
+                                       const char* actual_expression,
+                                       const QString & expected,
+                                       const char* actual) {
+            return CmpHelperSTREQ(expected_expression, actual_expression, expected.toUtf8().data(), actual);
+        }
+
+        inline
+        AssertionResult CmpHelperSTREQ(const char* expected_expression,
+                                       const char* actual_expression,
+                                       const QString & expected,
+                                       const QString & actual) {
+            return CmpHelperSTREQ(expected_expression, actual_expression, expected.toUtf8().data(), actual.toUtf8().data());
+        }
+
+        // Helper functions for ASSERT_STRNE / EXPECT_STRNE on QStrings.
+        inline
+        AssertionResult CmpHelperSTRNE(const char* expected_expression,
+                                       const char* actual_expression,
+                                       const char* expected,
+                                       const QString & actual) {
+            return CmpHelperSTRNE(expected_expression, actual_expression, expected, actual.toUtf8().data());
+        }
+
+        inline
+        AssertionResult CmpHelperSTRNE(const char* expected_expression,
+                                       const char* actual_expression,
+                                       const QString & expected,
+                                       const char* actual) {
+            return CmpHelperSTRNE(expected_expression, actual_expression, expected.toUtf8().data(), actual);
+        }
+
+        inline
+        AssertionResult CmpHelperSTRNE(const char* expected_expression,
+                                       const char* actual_expression,
+                                       const QString & expected,
+                                       const QString & actual) {
+            return CmpHelperSTRNE(expected_expression, actual_expression, expected.toUtf8().data(), actual.toUtf8().data());
+        }
+    }
+}
+
 #endif // HELPERS_H
 /// \} // end g_unittests
