7b5a220cb4ba770c5da2c9a614c9a63b21886274
Andrej Alpatov <inf3407@hs-worms.de>
2020-05-13 18:16:56 +0200

    P 6.1 add TEST_F

--- src/unittest/TestTile.cpp
+++ src/unittest/TestTile.cpp
@@ -10,6 +10,55 @@
 #include <gtest/gtest.h>
 //#include <iostream>
 
+class TestDefault2048 : public ::testing::Test{
+public:
+    TestDefault2048(){
+        cout<<"Konstruktor der Klasse TestDefault2048 ist aufgerufen"<< endl;
+    }
+    virtual ~TestDefault2048(){ cout<<"Destruktor der Klasse TestDefault2048"<<endl;}
+
+protected:
+    Tile field;
+
+};
+
+class TestTileGetter : public ::testing::TestWithParam<int>{
+
+};
+
+/// \test
+TEST_F(TestDefault2048,getTileFreiheitAndWert){//Praktikum 6.1
+
+    //Act && Assert
+    cout <<"Test getTileFreiheitAndWert ist aufgerufen"<<endl;
+    ASSERT_TRUE(field.getFrei()) ;
+    ASSERT_EQ(field.getWert(), 0);
+}
+
+/// \test
+TEST_F(TestDefault2048 ,setTileFreiheitAndWert) {//Praktikum 6.1
+    //Arrange
+    field.setFrei(false);
+
+    //Act && Assert
+
+    ASSERT_FALSE(field.getFrei()) ;
+    ASSERT_EQ(field.getWert(), 0);
+}
+
+TEST_P(TestTileGetter, returnesGiwenWert){//Praktikum 6.2
+
+    //setup
+    int wert=GetParam();
+    Tile tile(true, wert);
+
+    //verify
+    EXPECT_EQ(tile.getWert(), wert);
+
+}
+
+INSTANTIATE_TEST_SUITE_P(TileWert, TestTileGetter, ::testing::Values(0, 2, 2048));
+
 /// \test
 TEST(TestTile,getTileFreiheitAndWert) {
     //Arrange
